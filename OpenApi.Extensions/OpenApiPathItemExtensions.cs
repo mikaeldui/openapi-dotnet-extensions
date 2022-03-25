425b526665cce2c0a750ee3485ae0690af2b4937
Mikael Dúi Bolinder <github@bolinder.uk>
2022-03-26 00:04:43 +0100

    Add missing `this`.

--- OpenApi.Extensions/OpenApiPathItemExtensions.cs
+++ OpenApi.Extensions/OpenApiPathItemExtensions.cs
@@ -8,7 +8,7 @@ public static class OpenApiPathItemExtensions
     ///     Add one operation into this path item.
     /// </summary>
     /// <param name="operationType">Not case-sensitive.</param>
-    public static void AddOperation(OpenApiPathItem pathItem, string operationType, OpenApiOperation operation)
+    public static void AddOperation(this OpenApiPathItem pathItem, string operationType, OpenApiOperation operation)
     {
         pathItem.AddOperation(Enum.Parse<OperationType>(operationType, true), operation);
     }
